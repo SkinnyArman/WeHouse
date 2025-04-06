66b914532eb0cb242c70b55a9c614c9e83d1f133
Arman Amini <armanwithamini@gmail.com>
2025-04-06 12:08:53 +0330

    fix: make customerId in bannedCustomers unique

--- backend/src/models/BannedCustomer.ts
+++ backend/src/models/BannedCustomer.ts
@@ -3,7 +3,7 @@ import { IBannedCustomer } from '../types/bannedCustomer.types';
 
 const bannedCustomerSchema = new Schema<IBannedCustomer>(
   {
-    customerId: { type: String, required: true },
+    customerId: { type: String, required: true, unique: true },
     reason: { type: String, required: true }
   },
   {
